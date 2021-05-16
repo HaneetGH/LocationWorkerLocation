e6919e180d3f2a50a4abc3400d438c41647e7408
haneetsinghchhabra <HOTh@n!!t52p93>
2021-05-17 00:27:04 +0530

    change pk

--- app/src/androidTest/java/com/technorapper/workerexample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/technorapper/workerexample/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
 		// Context of the app under test.
 		Context appContext = InstrumentationRegistry.getTargetContext();
 
-		assertEquals("com.pratikbutani.workerexample", appContext.getPackageName());
+		assertEquals("com.technorapper.workerexample", appContext.getPackageName());
 	}
 }
